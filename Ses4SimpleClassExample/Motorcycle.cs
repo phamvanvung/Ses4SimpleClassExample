d1f2449cab1b316abc10174fc9f8db4b68513065
phamvanvung <phamvanvung@gmail.com>
2017-05-15 09:20:27 +0700

    Constructors with repeated code

--- Ses4SimpleClassExample/Motorcycle.cs
+++ Ses4SimpleClassExample/Motorcycle.cs
@@ -9,11 +9,22 @@ namespace Ses4SimpleClassExample
         {
 
         }
+        public Motorcycle(int intensity)
+        {
+            if(intensity > 10){
+                intensity = 10;
+            }
+            this.intensity = intensity;
+        }
         public Motorcycle(int intensity, string name)
         {
+            if(intensity > 10){
+                intensity = 10;
+            }
             this.intensity = intensity;
             this.name = name;
         }
+
         public void PopAWheely(){
             Console.WriteLine("Yeeeeeehaaaaaeeeee!");
         }
