59ddc7d1a35e9e13d0c60cbdfd1d0b2c7ea4bb01
phamvanvung <phamvanvung@gmail.com>
2017-05-15 08:44:46 +0700

    First Car without Constructor

---
+++ Ses4SimpleClassExample/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Ses4SimpleClassExample
+{
+    public class Car
+    {
+        //Common characteristics (variables)
+        public string petName;
+        public int currSpeed;
+        //Common behaviors (methods)
+        public void PrintState(){
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+        public void SpeedUp(int delta){
+            currSpeed += delta;
+        }
+    }
+}
---
+++ Ses4SimpleClassExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ses4SimpleClassExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.petName = "John";
+            c.currSpeed = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                c.PrintState();
+                c.SpeedUp(10);
+            }
+            Console.ReadLine();
+        }
+    }
+}
